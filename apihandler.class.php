e93e000c4382f1264bcf3b3fbff4c9459bda6d8b
Ari Asulin <aasulin@newaer.com>
2013-06-18 20:27:21 -0700

    Documentation

--- apihandler.class.php
+++ apihandler.class.php
@@ -14,34 +14,58 @@ use CPath\Models\Response;
 use CPath\Models\ResponseException;
 use CPath\Builders\BuildRoutes;
 
+/**
+ * Class ApiHandler
+ * @package CPath
+ *
+ * Provides a Handler template for API calls
+ */
 abstract class ApiHandler implements Interfaces\IHandler, Interfaces\IBuilder {
 
-    const BUILD_IGNORE = false;
+    const BUILD_IGNORE = false;     // API Calls are built to provide routes
 
-    const ROUTE_METHOD = 'GET';
-    const ROUTE_PATH = NULL;
+    const ROUTE_METHOD = 'GET';     // Default accepted method is GET
+    const ROUTE_PATH = NULL;        // No custom route path. Path is based on namespace + class name
 
-    private $mFields = array();
-
-    public function __construct() {
-    }
+    private $mFields = array();     // API Fields
 
     /**
-     * @param array $request associative array of request parameters, usually $_GET or $_POST
+     * Execute this API Endpoint with the entire request.
+     * This method must call processRequest to validate and process the request object.
+     * @param array $request associative array of request Fields, usually $_GET or $_POST
      * @return \CPath\Interfaces\IResponse the api call response with data, message, and status
      */
     abstract function execute(Array $request);
 
+    /**
+     * Add an API Field.
+     * @param $name the name of the Field
+     * @param IApiField $Field Describes the Field. Implement IApiField for custom validation
+     * @return $this Return the class instance
+     */
     protected function addField($name, IApiField $Field) {
         $this->mFields[$name] = $Field;
         return $this;
     }
+
+    /**
+     * Add an array of API Fields
+     * @param array $fields associative array of Fields.
+     * The array key represents the Field name.
+     * @return $this return the class instance
+     */
     protected function addFields(Array $fields) {
         foreach($fields as $name => $Field)
             $this->mFields[$name] = $Field;
         return $this;
     }
 
+    /**
+     * Process a request. Validates each Field. Provides optional Field formatting
+     * @param array $request the entire web request
+     * @return array the processed and validated request data
+     * @throws ValidationExceptions if one or more Fields fail to validate
+     */
     public function processRequest(Array $request) {
         $values = array();
         $FieldExceptions = new ValidationExceptions();
@@ -60,6 +84,10 @@ abstract class ApiHandler implements Interfaces\IHandler, Interfaces\IBuilder {
         return $values;
     }
 
+    /**
+     * Render this API Call. The output format is based on the requested mimeType from the browser
+     * @param array $args an array of arguments parsed out of the route path
+     */
     public function render(Array $args) {
         $request = strcasecmp(Util::getUrl('method'), 'get') === 0
             ? $_GET
@@ -124,6 +152,10 @@ abstract class ApiHandler implements Interfaces\IHandler, Interfaces\IBuilder {
         }
     }
 
+    /**
+     * Provides the formatted route for viewing purposes
+     * @return string the formatted route
+     */
     public function getDisplayRoute() {
         $route = Build::getHandlerRoute($this);
         foreach($this->mFields as $name => $Field) {
@@ -136,29 +168,53 @@ abstract class ApiHandler implements Interfaces\IHandler, Interfaces\IBuilder {
 
     // Statics
 
+    /** Builds the API Endpoint route */
     public static function build(\ReflectionClass $Class) {
         BuildRoutes::build($Class);
     }
 
+    /** Processes the API Endpoint route into the routes file */
     public static function buildComplete(\ReflectionClass $Class) {
         BuildRoutes::buildComplete($Class);
     }
 }
 
+/**
+ * Class ValidationException
+ * @package CPath
+ * Thrown when a Field fails to validate
+ */
 class ValidationException extends \Exception {
     public function getFieldError($fieldName) {
         return sprintf($this->getMessage(), $fieldName);
     }
 }
+
+/**
+ * Class ValidationExceptions
+ * @package CPath
+ * Throw when one or more Fields fails to validate
+ */
 class ValidationExceptions extends MultiException {
     public function addFieldException($fieldName, ValidationException $ex) {
         parent::add($ex->getFieldError($fieldName));
     }
 }
+
+/**
+ * Class IApiField
+ * @package CPath
+ * Represents an API Field
+ */
 interface IApiField {
     public function validate($value);
 }
 
+/**
+ * Class ApiOptionalField
+ * @package CPath
+ * Represents an 'optional' API Field
+ */
 class ApiOptionalField implements IApiField {
     public $mDescription;
     public function __construct($description=NULL) {
@@ -170,9 +226,20 @@ class ApiOptionalField implements IApiField {
     }
 }
 
+/**
+ * Class RequiredFieldException
+ * @package CPath
+ * Throw when a required field is missing
+ */
 class RequiredFieldException extends ValidationException {
     public function getFieldError($fieldName) { return "Field '{$fieldName}' is required."; }
 }
+
+/**
+ * Class ApiRequiredField
+ * @package CPath
+ * Represents a 'required' API Field
+ */
 class ApiRequiredField extends ApiOptionalField {
     public function validate($value) {
         if(!$value && $value !== '0')
@@ -181,6 +248,11 @@ class ApiRequiredField extends ApiOptionalField {
     }
 }
 
+/**
+ * Class ApiParam
+ * @package CPath
+ * Represents a Parameter from the route path
+ */
 class ApiParam extends ApiRequiredField {
 
 }
--- base.class.php
+++ base.class.php
@@ -7,13 +7,21 @@
  * Date: 4/06/11 */
 namespace CPath;
 
+/**
+ * Class Base
+ * @package CPath
+ *
+ * Provides required framework functionality such as class autoloader and directories
+ */
 class Base {
     private static $mLoaded, $mBasePath, $mLog = array(), $mDebug = false;
 
+    /** Initialize Static Class on include */
     public static function init() {
         self::$mBasePath = dirname(__DIR__) . "/";
     }
 
+    /** Activate Autoloader for classes */
     public static function load() {
         if(!self::$mLoaded) {
             spl_autoload_register(__NAMESPACE__.'\Base::loadClass', true);
@@ -21,11 +29,13 @@ class Base {
         }
     }
 
+    /** Deactivate Autoloader for classes */
     public static function unload() {
         spl_autoload_unregister(__NAMESPACE__.'\Base::loadClass');
         self::$mLoaded = false;
     }
 
+    /** Autoloader for classes. Path matches namespace heirarchy of Class */
     private static function loadClass($name) {
         if(strpos($name, '\\')===false) return;
         //$name = str_replace('\\', '/', strtolower($name));
@@ -34,35 +44,55 @@ class Base {
         include_once($classPath);
     }
 
+    /** Attempt to route a web request to it's destination */
     public static function routeRequest() {
         self::load();
         Route::tryAllRoutes();
     }
 
+    /** Returns the path to the project directory */
     public static function getBasePath() {
         return static::$mBasePath;
     }
 
+    /** Returns the path to the generated files directory */
     public static function getGenPath() {
         static $gen = NULL;
         return $gen ?: $gen = self::getBasePath().'gen/';
     }
 
+    /**
+     * Log a message
+     * @param $msg the message to log
+     */
     public static function log($msg) {
         self::$mLog[] = $msg;
     }
 
+    /**
+     * Return the entire log
+     * @return array a list of log entries
+     */
     public static function getLog() {
         return self::$mLog;
     }
 
+    /**
+     * Returns true if debug mode is set
+     * @return bool true if debug mode is set
+     */
     public static function isDebug() {
         return self::$mDebug;
     }
 
+    /**
+     * Set debug mode on and off
+     * @param bool $debug set to true to enable debug mode
+     */
     public static function setDebug($debug=true) {
         self::$mDebug = $debug;
     }
 }
 
+// Static class initializes on include
 Base::init();
