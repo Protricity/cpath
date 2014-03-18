529fa8bb8fbf8453ef6138b9ae1aab4f96a5dddb
Ari Asulin <ari@newaer.com>
2014-03-18 09:26:18 -0700

    Build Test

--- Tests/BuildTest.php
+++ Tests/BuildTest.php
@@ -10,11 +10,6 @@
 
 include_once __DIR__ . '/../Base.php';
 
-use API\DB\Model\ShareDeviceEntityModel as SDE;
-use API\DB\Model\ShareDeviceModel as SD;
-use API\DB\Model\ShareWapEntityModel as SWE;
-use API\DB\Model\ShareWapModel as SW;
-use API\DB\Model\ShareWapNetworkHashModel as SWNH;
 use CPath\Misc\ApiTester as Test;
 
 class BuildTest extends PHPUnit_Framework_TestCase {
